1a5652a7b2c06c4350f15817039a882b0da776ee
Staffan Frisk <tech@evol.se>
2023-05-10 20:40:08 +0200

    added a slow test to prove the point

--- src/test/java/se/evol/spring6withwebflux/controllers/EmployerControlControllerTest.java
+++ src/test/java/se/evol/spring6withwebflux/controllers/EmployerControlControllerTest.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -94,5 +91,25 @@ class EmployerControlControllerTest {
                         .withBody(registryBody))
         );
         webTestClient.get().uri(EmployerControlController.VALIDATION_PATH_EARLY_EXIT + failingId).exchange().expectBody(String.class).isEqualTo("{\"isValidOnTax\":false,\"isValidOnRegistration\":true}");
+    }
+    @Test
+    // @Disabled
+    @DisplayName("Slow when both returns true")
+    void testValidationThatIsSlowToProveAPoint () throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String taxBody = mapper.writeValueAsString(TaxValidationResponse.builder().fSkatt("true").build());
+        wireMockServer.stubFor(get(urlEqualTo(TAX_PATH + failingId))
+                .willReturn(ok()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(taxBody))
+        );
+        String registryBody = mapper.writeValueAsString(RegistryValidationResponse.builder().isValid("true").build());
+        wireMockServer.stubFor(get(urlEqualTo(REGISTRY_PATH + failingId))
+                .willReturn(ok()
+                        .withFixedDelay(3000)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(registryBody))
+        );
+        webTestClient.get().uri(EmployerControlController.VALIDATION_PATH_EARLY_EXIT + failingId).exchange().expectBody(String.class).isEqualTo("{\"isValidOnTax\":true,\"isValidOnRegistration\":true}");
     }
 }
